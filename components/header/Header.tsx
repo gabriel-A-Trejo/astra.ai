1c603f57a1bb642528ea0af159bb4cd3185b7bb4
Gabriel A Trejo <GabrielTrejo433@gmail.com>
2025-04-08 08:15:22 -0500

    feat(header): enhance header with logo and styling
    Add Logo component and improve header layout with gradient background, responsive padding, and hover effects. This enhances the visual appeal and user experience of the header section.

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,7 +1,24 @@
+import Link from "next/link";
 import React from "react";
+import Logo from "./Logo";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="fixed top-0 right-0 left-0 z-50 py-2">
+      <div className="absolute inset-0 bg-gradient-to-b to-transparent from-black/80 via-black/40"></div>
+      <div className="relative  px-12 py-4 mx-auto max-w-8xl">
+        <div className="flex justify-between items-center">
+          <Link
+            href="/"
+            aria-label="Astra AI Home"
+            className="flex items-center  transition-opacity hover:opacity-80"
+          >
+            <Logo />
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
