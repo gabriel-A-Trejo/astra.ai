a14b9048519112b4263ab01392660b5159e1b621
Gabriel A Trejo <GabrielTrejo433@gmail.com>
2025-06-02 13:24:40 -0500

    refactor(layout): remove unused Metadata import from root layout
    The Metadata import was not being used in the file, so it was removed to clean up the code and improve maintainability.

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,4 @@
 import Header from "@/components/header/Header";
-import { Metadata } from "next/dist/lib/metadata/types/metadata-interface";
 import React from "react";
 
 export default function RootLayout({
