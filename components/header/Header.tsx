0b9a959765b9cc02287fb6aaa08cc2962f241868
Gabriel A Trejo <GabrielTrejo433@gmail.com>
2025-06-09 10:18:22 -0500

    style(header): remove fixed positioning from header
    The fixed positioning was causing layout issues with other elements

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -4,7 +4,7 @@ import AuthButton from "./AuthButton";
 
 const Header = () => {
   return (
-    <header className="fixed top-0 left-0 right-0 z-50 p-4">
+    <header className=" top-0 left-0 right-0 z-50 p-4">
       <section className="flex justify-between items-center mx-auto ">
         <Logo />
         <nav className="flex gap-3">
